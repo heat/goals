4c4391e66624a3ee45bb0359833986aceb275101
Leandro Neto <lndro.neto@gmail.com>
2016-10-20 21:15:18 -0300

    Removendo super

--- app/validations/GoalsException.java
+++ app/validations/GoalsException.java
@@ -6,7 +6,7 @@ public class GoalsException extends Exception{
     public String message;
 
     public GoalsException(Integer code, String message) {
-        super(message);
+        this.message = message;
         this.code = code;
     }
 }
