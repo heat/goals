c1b01692d8cb85512dd7a09740beb21e5b96fb70
Leandro Neto <lndro.neto@gmail.com>
2016-10-25 23:13:12 -0300

    Adicionando campos para a persistência de metas

--- app/domain/converters/MetaConverter.java
+++ app/domain/converters/MetaConverter.java
@@ -9,8 +9,12 @@ public abstract class MetaConverter {
     public static MetaEntity domainToEntity(Meta domain){
         MetaEntity entity = new MetaEntity();
 
-        entity.nome = domain.getNome();
         entity.id_usuario = domain.getId_usuario();
+//        entity.id_categoria = domain.getCategoria().getId();
+        entity.nome = domain.getNome();
+        entity.descricao = domain.getDescricao();
+        /*entity.dta_fim = domain.getDataFim();
+        entity.dta_inicio = domain.getDataInicio();*/
 
         return entity;
     }
--- app/infrastructure/entities/MetaEntity.java
+++ app/infrastructure/entities/MetaEntity.java
@@ -1,6 +1,7 @@
 package infrastructure.entities;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "metas")
@@ -9,6 +10,10 @@ public class MetaEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Integer id_meta;
-    public String nome;
     public Integer id_usuario;
+    public Integer id_categoria;
+    public String nome;
+    public String descricao;
+    public LocalDate dta_inicio;
+    public LocalDate dta_fim;
 }
